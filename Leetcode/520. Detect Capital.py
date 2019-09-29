20ceeadf25b3b233c0f7331eeaef4ced3e450eb1
chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2019-09-29 18:08:17 +0800

    Add files via upload

---
+++ Leetcode/520. Detect Capital.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper():
+            return True
+        elif word.islower():
+            return True
+        elif word[1:].islower():
+            return True
+        else:
+            return False
