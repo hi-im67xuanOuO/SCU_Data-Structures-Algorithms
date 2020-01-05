0c945dd263b7d37f095c91b0fa77ebb0b7d1b132
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:48:15 +0800

    Delete 3. chekPalindrome.py

--- codesignal - intro/3. chekPalindrome.py
+++
@@ -1,9 +0,0 @@
-def checkPalindrome(inputString):
-    # For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
-    # For inputString = "abac", the output should be checkPalindrome(inputString) = false;
-    # For inputString = "a", the output should be checkPalindrome(inputString) = true.
-    l = len(inputString)
-    for i in range(l):
-        if inputString[i] != inputString[-1-i]:
-            return False
-    return True
