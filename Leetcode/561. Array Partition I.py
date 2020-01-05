98f65809e8e606ad8156bf4b45c2575f7fbddbe6
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 02:19:26 +0800

    Delete 561. Array Partition I.py

--- Leetcode/561. Array Partition I.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        ans = 0
-        num = sorted(nums)
-        for i in range(0,len(num)-1,2):
-            ans += min(num[i],num[i+1])
-        return ans
