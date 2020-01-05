403297ef2544e4d7fda7c9a831b704a295473f37
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:46:52 +0800

    Delete 10. commonCharacterCount.py

--- codesignal - intro/10. commonCharacterCount.py
+++
@@ -1,7 +0,0 @@
-def commonCharacterCount(s1, s2):
-    # For s1 = "aabcc" and s2 = "adcaa", the output should be commonCharacterCount(s1, s2) = 3. Strings have 3 common characters - 2 "a"s and 1 "c".
-    x = 0
-    for i in set(s1):
-        m = min(s1.count(i),s2.count(i))
-        x += m
-    return x
