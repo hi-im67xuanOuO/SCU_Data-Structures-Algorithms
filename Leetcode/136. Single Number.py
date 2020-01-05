db14629966ac2e36dbda0c7807d6a59a03b5f26f
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:59:07 +0800

    Delete 136. Single Number.py

--- Leetcode/136. Single Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        a = []
-        for i in nums:
-            if i not in a:
-                a.append(i)
-            else:
-                a.remove(i)
-        return a.pop()
