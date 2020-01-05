ec6c7c0871aa0c5a1d3393a35462f1505aba07ba
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 02:00:10 +0800

    Delete 217. Contains Duplicate.py

--- Leetcode/217. Contains Duplicate.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        a = sorted(nums)
-        for i in range(len(a)-1):
-            if a[i] == a[i+1]:
-                return True
-        return False
