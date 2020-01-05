e2425ff721aad87b3db435fb873663b04dc934d1
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:59:38 +0800

    Delete 27. Remove Element.py

--- Leetcode/27. Remove Element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        n = 0
-        while n<len(nums):
-            if val == nums[n]:
-                nums.pop(n)
-            else:
-                n+=1
-        return len(nums)
