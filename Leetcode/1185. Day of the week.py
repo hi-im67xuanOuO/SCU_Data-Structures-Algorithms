1f6d3aa89a36a4cf73d937923cbce67f1b921e33
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:58:41 +0800

    Delete 1185. Day of the week.py

--- Leetcode/1185. Day of the week.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-class Solution:
-    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        ddate = date(year,month,day)
-        week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        return week[ddate.weekday()]
