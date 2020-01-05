f77bea56291cab579baf86aa8e537ba4214b6400
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:49:57 +0800

    Delete 14. convertTabs.py

--- codesignal - python/14. convertTabs.py
+++
@@ -1,6 +0,0 @@
-def convertTabs(code, x):
-    # given a piece of code and a positive integer x will turn each tabulation character in code into x whitespace characters.
-    
-    # For code = "\treturn False" and x = 4, the output should be convertTabs(code, x) = "    return False"
-    
-    return code.replace('\t', ' ' * x)
