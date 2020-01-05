8261443ac573ed9a21cb3e7bbf87e823a6b47014
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:59:46 +0800

    Delete 242. Valid Anagram.py

--- Leetcode/242. Valid Anagram.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        a = sorted(s)
-        b = sorted(t)
-        if a == b:
-            return True
-        else:
-            return False
