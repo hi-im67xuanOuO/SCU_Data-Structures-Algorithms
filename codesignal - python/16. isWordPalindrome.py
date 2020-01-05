36e17d370e989c243d5da3a6748be6dea520d0af
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:50:36 +0800

    Delete 16. isWordPalindrome.py

--- codesignal - python/16. isWordPalindrome.py
+++
@@ -1,5 +0,0 @@
-def isWordPalindrome(word):
-    # For word = "aibohphobia", the output should be isWordPalindrome(word) = true;
-    # For word = "hehehehehe", the output should be isWordPalindrome(word) = false.
-
-    return word == word[::-1]
