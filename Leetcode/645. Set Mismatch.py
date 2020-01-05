273247c62e456822221284b33d83cb209392f978
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 02:20:38 +0800

    Delete 645. Set Mismatch.py

--- Leetcode/645. Set Mismatch.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        holder = [0]*len(nums)
-        repeat_num = 0
-        for i in range(len(nums)):
-            if holder[nums[i]-1] != 0:
-                repeat_num = nums[i]
-            holder[nums[i]-1] = nums[i]
-        return [repeat_num,holder.index(0)+1]
