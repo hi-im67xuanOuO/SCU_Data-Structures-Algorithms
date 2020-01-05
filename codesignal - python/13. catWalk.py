b14a5b129667cdcf10e609056e5a8ebf5fb056d6
Chinghsuan <38979961+chinghsuan@users.noreply.github.com>
2020-01-06 01:49:48 +0800

    Delete 13. catWalk.py

--- codesignal - python/13. catWalk.py
+++
@@ -1,6 +0,0 @@
-def catWalk(code):
-    # replace all multiple space characters in the given line of your code with single ones. In addition, all leading and trailing whitespaces should be removed.
-    
-    #For line = "def      m   e  gaDifficu     ltFun        ction(x):", the output should be catWalk(line) = "def m e gaDifficu ltFun ction(x):".
-    
-    return " ".join(code.split())
